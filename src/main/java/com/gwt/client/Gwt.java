e4c6a4629da76c442c52b639cfd5cc3354ba3590
Serhii Zahorui <zahoruiserhii@gmail.com>
2020-05-20 17:25:29 +0300

    Clean code

--- src/main/java/com/gwt/client/Gwt.java
+++ src/main/java/com/gwt/client/Gwt.java
@@ -20,7 +20,7 @@ public class Gwt implements EntryPoint {
 
 	// Intro Screen widgets
 	private VerticalPanel introScreen = new VerticalPanel();
-	private VerticalPanel contentWrapper1 = new VerticalPanel();
+	private VerticalPanel inputFieldsWrapper = new VerticalPanel();
 	private Label introScreenTitle = new Label("Intro screen");
 	private Label label = new Label("How many numbers to display?");
 	private TextBox inputField = new TextBox();
@@ -29,7 +29,7 @@ public class Gwt implements EntryPoint {
 
 	// Sorts Screen widgets
 	private VerticalPanel sortsScreen = new VerticalPanel();
-	private HorizontalPanel contentWrapper2 = new HorizontalPanel();
+	private HorizontalPanel contentWrapper = new HorizontalPanel();
 	private Label sortsScreenTitle = new Label("Sorts screen");
 	private HorizontalPanel leftSide = new HorizontalPanel();
 	private VerticalPanel rightSide = new VerticalPanel();
@@ -57,40 +57,37 @@ public class Gwt implements EntryPoint {
 		configSortsScreen();
 		RootPanel.get().add(introScreen);
 		RootPanel.get().add(sortsScreen);
-
 	}
 
 	private void configIntroScreen() {
 		introScreen.setStyleName("introScreen");
 		introScreen.add(introScreenTitle);
-		introScreen.add(contentWrapper1);
+		introScreen.add(inputFieldsWrapper);
 
 		introScreenTitle.setStyleName("screenTitle");
 
-		contentWrapper1.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		contentWrapper1.setSpacing(10);
-		contentWrapper1.add(label);
-		contentWrapper1.add(inputField);
-		contentWrapper1.add(enterButton);
-		contentWrapper1.add(errorMessage);
-		contentWrapper1.setStyleName("contentWrapper");
+		inputFieldsWrapper.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		inputFieldsWrapper.setSpacing(10);
+		inputFieldsWrapper.add(label);
+		inputFieldsWrapper.add(inputField);
+		inputFieldsWrapper.add(enterButton);
+		inputFieldsWrapper.add(errorMessage);
+		inputFieldsWrapper.setStyleName("contentWrapper");
 
 		errorMessage.setVisible(false);
 		enterButton.addClickHandler(new EnterButtonHandler());
-
 	}
 
 	private void configSortsScreen() {
-
 		sortsScreen.addStyleName("sortsScreen");
 		sortsScreen.add(sortsScreenTitle);
-		sortsScreen.add(contentWrapper2);
+		sortsScreen.add(contentWrapper);
 
 		sortsScreenTitle.setStyleName("screenTitle");
 
-		contentWrapper2.setStyleName("contentWrapper");
-		contentWrapper2.add(leftSide);
-		contentWrapper2.add(rightSide);
+		contentWrapper.setStyleName("contentWrapper");
+		contentWrapper.add(leftSide);
+		contentWrapper.add(rightSide);
 
 		sortsScreen.setVisible(false);
 
@@ -107,7 +104,6 @@ public class Gwt implements EntryPoint {
 		resetButton.addStyleName("resetButton");
 
 		popUp.add(restrictMessage);
-
 	}
 
 	private class EnterButtonHandler implements ClickHandler {
@@ -121,9 +117,7 @@ public class Gwt implements EntryPoint {
 				errorMessage.setVisible(false);
 
 				int randomNumbersAmount = Integer.valueOf(input);
-				generateRandomNumbers(randomNumbersAmount);                        /* 2 */
-				randomNumbersCopy.addAll(randomNumbers);
-				numerateButtons();
+				generateRundomNumbersList(randomNumbersAmount);
 
 				Timer timer = new Timer() {
 
@@ -133,8 +127,8 @@ public class Gwt implements EntryPoint {
 					}
 
 				};
-				registerAndSchedule(timer, 800, false);
 
+				registerAndSchedule(timer, 800, false);
 			} else {
 				errorMessage.setVisible(true);
 			}
@@ -142,22 +136,18 @@ public class Gwt implements EntryPoint {
 		}
 
 		private boolean isValidInput(String input) {
-
 			if (input.isEmpty() || (input.length() > 4) || (input.charAt(0) == '0')) {
 				return false;
 			}
-
 			// Check whether input string is a number
 			for (int i = 0; i < input.length(); i++) {
 				if (!Character.isDigit(input.charAt(i))) {
 					return false;
 				}
 			}
-
 			if (Integer.valueOf(input) > 1000) {
 				return false;
 			}
-
 			return true;
 		}
 
@@ -169,15 +159,7 @@ public class Gwt implements EntryPoint {
 		public void onClick(ClickEvent event) {
 			sortsScreen.setVisible(false);
 			introScreen.setVisible(true);
-
-			inputField.setText("");
-
-			resetTimers();
-			numberedButtons.clear();
-			randomNumbers.clear();                                          /* 1 */
-			randomNumbersCopy.clear();
-			leftSide.clear();
-			popUp.hide();
+			clearAll();
 		}
 
 	}
@@ -187,12 +169,6 @@ public class Gwt implements EntryPoint {
 
 		@Override
 		public void onClick(ClickEvent event) {
-			popUp.hide();
-			randomNumbersCopy.clear();
-			leftSide.clear();                                               /* 1 */
-			numberedButtons.clear();
-			resetTimers();
-
 			if (order.equals("asc")) {
 				order = "desc";
 			} else if (order.equals("desc")) {
@@ -208,14 +184,12 @@ public class Gwt implements EntryPoint {
 
 			};
 
-			randomNumbersCopy.addAll(randomNumbers);                                         /* 2 */
-			numerateButtons();
-
+			clear();
+			copyRandomNumbersList();
 			registerAndSchedule(render, BUTTONS_DISPLAY_INTERVAL, true);
-			/* timersList.add(render) - хз нада ця строка її тут не було */
-
-			registerAndSchedule(timer, BUTTONS_DISPLAY_INTERVAL * randomNumbers.size() + 20, false);
-
+			// star this timer when previous time is complete
+			int numberOfButtons = numberedButtons.size();
+			registerAndSchedule(timer, BUTTONS_DISPLAY_INTERVAL * numberOfButtons + 20, false);
 		}
 
 	}
@@ -224,26 +198,12 @@ public class Gwt implements EntryPoint {
 
 		@Override
 		public void onClick(ClickEvent event) {
-
 			Button theButton = (Button) event.getSource();
 			int buttonValue = Integer.parseInt(theButton.getText());
 			if ((buttonValue != 0) && (buttonValue <= 30)) {
-
-				// Clean all values in lists
-				resetTimers();
-				numberedButtons.clear();
-				randomNumbers.clear();                                                   /* 1 */
-				randomNumbersCopy.clear();
-				leftSide.clear();
-				popUp.hide();
-
-				// Generate new values
-				generateRandomNumbers(buttonValue);                                       /* 2 */
-				randomNumbersCopy.addAll(randomNumbers);
-				numerateButtons();
-
+				clearAll();
+				generateRundomNumbersList(buttonValue);
 				registerAndSchedule(render, BUTTONS_DISPLAY_INTERVAL, true);
-
 			} else {
 				int left = theButton.getAbsoluteLeft() + 10;
 				int top = theButton.getAbsoluteTop() + 10;
@@ -260,131 +220,121 @@ public class Gwt implements EntryPoint {
 				};
 
 				registerAndSchedule(timer, 1000, false);
-
 			}
 
 		}
 
 	}
 
-	private void resetTimers() {
-		for (int i = 0; i < timersList.size(); i++) {
-			timersList.get(i).cancel();
-		}
-		timersList.clear();
-	}
-
 	private void quickSort(String order) {
 		if (order.equals("asc")) {
-			ASCQuickSortTimer aqst = new ASCQuickSortTimer(0, randomNumbers.size() - 1);
+			AscQuickSorter aqst = new AscQuickSorter(0, randomNumbers.size() - 1);
 			registerAndSchedule(aqst, SORTING_ITERATION_DELAY, false);
 		}
-
 		if (order.equals("desc")) {
-			DESCQuickSortTimer dqst = new DESCQuickSortTimer(0, randomNumbers.size() - 1);
+			DescQuickSorter dqst = new DescQuickSorter(0, randomNumbers.size() - 1);
 			registerAndSchedule(dqst, SORTING_ITERATION_DELAY, false);
 		}
 
 	}
 
-	private class ASCQuickSortTimer extends Timer {
+	private class AscQuickSorter extends Timer {
 		private int low;
 		private int high;
 
-		public ASCQuickSortTimer(int low, int high) {
+		public AscQuickSorter(int low, int high) {
 			this.low = low;
 			this.high = high;
 		}
 
 		@Override
 		public void run() {
-			int i = low, j = high;
+			int i = low;
+			int j = high;
 			// Get the pivot element from the middle of the list
 			int pivot = randomNumbersCopy.get(low + (high - low) / 2);
-
 			// Divide into two lists
 			while (i <= j) {
-				// If the current value from the left list is smaller than the pivot
-				// element then get the next element from the left list
+				/*
+				 * If the current value from the left list is smaller than the pivot element
+				 * then get the next element from the left list
+				 */
 				while (randomNumbersCopy.get(i) < pivot) {
 					i++;
 				}
-				// If the current value from the right list is larger than the pivot
-				// element then get the next element from the right list
+				/*
+				 * If the current value from the right list is larger than the pivot element
+				 * then get the next element from the right list
+				 */
 				while (randomNumbersCopy.get(j) > pivot) {
 					j--;
 				}
-
-				// If we have found a value in the left list which is larger than
-				// the pivot element and if we have found a value in the right list
-				// which is smaller than the pivot element then we swap the
-				// values.
-				// As we are done we can increase i and j
+				/*
+				 * If we have found a value in the left list which is larger than the pivot
+				 * element and if we have found a value in the right list which is smaller than
+				 * the pivot element then we swap the values. As we are done we can increase i
+				 * and j
+				 */
 				if (i <= j) {
 					swap(i, j);
-					swapButtonText(i, j);
+					swapButtonsText(i, j);
 					i++;
 					j--;
 				}
 			}
 			// Recursion
 			if (low < j) {
-				ASCQuickSortTimer qst1 = new ASCQuickSortTimer(low, j);
-				registerAndSchedule(qst1, SORTING_ITERATION_DELAY, false);
+				AscQuickSorter aqs = new AscQuickSorter(low, j);
+				registerAndSchedule(aqs, SORTING_ITERATION_DELAY, false);
 			}
-
 			if (i < high) {
-				ASCQuickSortTimer qst2 = new ASCQuickSortTimer(i, high);
-				registerAndSchedule(qst2, SORTING_ITERATION_DELAY, false);
+				AscQuickSorter aqs = new AscQuickSorter(i, high);
+				registerAndSchedule(aqs, SORTING_ITERATION_DELAY, false);
 			}
 
 		}
 
 	}
 
-	private class DESCQuickSortTimer extends Timer {
+	private class DescQuickSorter extends Timer {
 		private int low;
 		private int high;
 
-		public DESCQuickSortTimer(int low, int high) {
+		public DescQuickSorter(int low, int high) {
 			this.low = low;
 			this.high = high;
 		}
 
 		@Override
 		public void run() {
-			int i = low, j = high;
+			int i = low;
+			int j = high;
 			// Get the pivot element from the middle of the list
 			int pivot = randomNumbersCopy.get(low + (high - low) / 2);
-
 			// Divide into two lists
 			while (i <= j) {
 				while (randomNumbersCopy.get(i) > pivot) {
 					i++;
 				}
-
 				while (randomNumbersCopy.get(j) < pivot) {
 					j--;
 				}
-
 				if (i <= j) {
 					swap(i, j);
-					swapButtonText(i, j);
+					swapButtonsText(i, j);
 					i++;
 					j--;
 				}
 			}
 			// Recursion
 			if (low < j) {
-				DESCQuickSortTimer dqst1 = new DESCQuickSortTimer(low, j);
-				registerAndSchedule(dqst1, SORTING_ITERATION_DELAY, false);
+				DescQuickSorter dqs = new DescQuickSorter(low, j);
+				registerAndSchedule(dqs, SORTING_ITERATION_DELAY, false);
 			}
-
 			if (i < high) {
-				DESCQuickSortTimer dqst2 = new DESCQuickSortTimer(i, high);
-				registerAndSchedule(dqst2, SORTING_ITERATION_DELAY, false);
+				DescQuickSorter dqs = new DescQuickSorter(i, high);
+				registerAndSchedule(dqs, SORTING_ITERATION_DELAY, false);
 			}
-
 		}
 
 	}
@@ -395,29 +345,30 @@ public class Gwt implements EntryPoint {
 		randomNumbersCopy.set(j, temp);
 	}
 
-	private void swapButtonText(int i, int j) {
+	private void swapButtonsText(int i, int j) {
 		String temp = numberedButtons.get(i).getText();
 		numberedButtons.get(i).setText(numberedButtons.get(j).getText());
 		numberedButtons.get(j).setText(temp);
 	}
 
-	private void generateRandomNumbers(int amount) {
+	private void generateRandomNumbers(int numbersAmount) {
 		boolean hasNumber = false;
 		int randomNumber;
-		for (int i = 0; i < amount; i++) {
+		for (int i = 0; i < numbersAmount; i++) {
 			randomNumber = com.google.gwt.user.client.Random.nextInt(1000);
 			randomNumbers.add(randomNumber);
-			
 			// Check whether there is at list one number that is <= 30 in the list
-			if (randomNumber <= 30) {
+			if ((randomNumber <= 30) && (randomNumber != 0)) {
 				hasNumber = true;
 			}
 		}
 
 		if (!hasNumber) {
 			// In case there isn't such number in list - generate and set it
-			randomNumber = com.google.gwt.user.client.Random.nextInt(30);
-			int position = com.google.gwt.user.client.Random.nextInt(amount);
+			int position = com.google.gwt.user.client.Random.nextInt(numbersAmount);
+			randomNumber = com.google.gwt.user.client.Random.nextInt(31);
+			if (randomNumber == 0)
+				randomNumber = 1;
 			randomNumbers.set(position, randomNumber);
 		}
 
@@ -439,20 +390,16 @@ public class Gwt implements EntryPoint {
 
 		@Override
 		public void run() {
-
 			// create new column
 			if ((i % 10) == 0) {
 				vPanel = new VerticalPanel();
 				vPanel.setStyleName("column");
 				leftSide.add(vPanel);
 			}
-
 			// put button in column
 			vPanel.add(numberedButtons.get(i));
-
 			// pick next button
 			i++;
-
 			// Reached end of numberedButtons list
 			if (i == numberedButtons.size()) {
 				i = 0;
@@ -461,6 +408,37 @@ public class Gwt implements EntryPoint {
 			}
 		}
 
+	}
+
+	private void clearAll() {
+		clear();
+		inputField.setText("");
+		randomNumbers.clear();
+	}
+
+	private void clear() {
+		popUp.hide();
+		resetTimers();
+		randomNumbersCopy.clear();
+		numberedButtons.clear();
+		leftSide.clear();
+	}
+
+	private void resetTimers() {
+		for (int i = 0; i < timersList.size(); i++) {
+			timersList.get(i).cancel();
+		}
+		timersList.clear();
+	}
+
+	private void generateRundomNumbersList(int numbersAmount) {
+		generateRandomNumbers(numbersAmount);
+		copyRandomNumbersList();
+	}
+
+	private void copyRandomNumbersList() {
+		randomNumbersCopy.addAll(randomNumbers);
+		numerateButtons();
 	}
 
 	private void registerAndSchedule(Timer timer, int interval, boolean repeating) {
