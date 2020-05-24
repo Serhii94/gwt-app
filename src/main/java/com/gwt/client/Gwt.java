71b96e59351d665df1d953e496dd7dcb4eca3f37
Serhii Zahorui <zahoruiserhii@gmail.com>
2020-05-24 17:37:45 +0300

    Removed redundant constant declaration

--- src/main/java/com/gwt/client/Gwt.java
+++ src/main/java/com/gwt/client/Gwt.java
@@ -46,7 +46,6 @@ public class Gwt implements EntryPoint {
 
     // Sorting components configs
     private String sortOrder = "desc";
-    private static final int BUTTONS_DISPLAY_INTERVAL = 20;
     private static final int SORTING_ITERATION_INTERVAL = 50;
 
     // Utility objects and containers
